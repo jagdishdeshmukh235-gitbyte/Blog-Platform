ddb50216e99de10e2c869ebed8460e82bb21c87a
JAGDISH <JAGDISHDESHMUKH235@GMAIL.COM>
2026-04-05 16:20:01 +0530

    deployment ready

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def search():
     return render_template("search.html", results=results, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
